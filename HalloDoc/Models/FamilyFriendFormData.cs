e0273b940ba8f01ee772d1b8e1748ec14ce91c3a
Shlok-tatva <shlok.jadeja@etatvasoft.com>
2024-02-08 18:21:28 +0530

    Add businessData Model

---
+++ HalloDoc/Models/FamilyFriendFormData.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HalloDoc.Models
+{
+    public class FamilyFriendFormData : PatientFormData
+    {
+        [Required (ErrorMessage = "Please enter first name")]
+        public string f_firstName { get; set; }
+
+        [Required (ErrorMessage = "Please enter last name")]
+        public string f_lastName { get; set; }
+
+        [Required (ErrorMessage = "Please enter email")]
+        public string f_Email { get; set; }
+
+        [Required (ErrorMessage = "Please enter Phone Number")]
+        public string f_PhoneNumber { get; set; }
+
+        [Required (ErrorMessage = "Please enter relation")]
+        public string relationWithPatinet { get; set; }
+
+    }
+}
