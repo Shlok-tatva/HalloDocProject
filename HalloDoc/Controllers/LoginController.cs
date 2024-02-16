7f03ba7b748b78b5863a83020f992833d67d0867
Shlok-tatva <shlok.jadeja@etatvasoft.com>
2024-02-16 18:00:23 +0530

    Forget Password Logic and UI Done with Mail sent

--- HalloDoc/Controllers/LoginController.cs
+++ HalloDoc/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using HalloDoc_BAL.Interface;
+﻿using Azure.Core;
+using HalloDoc_BAL.Interface;
 using HalloDoc_DAL.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,10 +9,14 @@ namespace HalloDoc.Controllers
     public class LoginController : Controller
     {
         private readonly IAspnetuserRepository _aspnetuserrepo;
+        private readonly IPatientFunctionRepository _petientfunctionrepo;
+        private readonly IUserRepository _userrepo;
 
-        public LoginController(IAspnetuserRepository aspnetuser)
+        public LoginController(IAspnetuserRepository aspnetuser, IPatientFunctionRepository petientfunctionrepo ,IUserRepository userrepo)
         {
             _aspnetuserrepo = aspnetuser;
+            _petientfunctionrepo = petientfunctionrepo;
+            _userrepo = userrepo;
         }
 
         public IActionResult Index()
@@ -56,6 +61,47 @@ namespace HalloDoc.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult resetPassword(string email)
+        {
+            User user = _userrepo.GetUser(email);
+            if(user == null)
+            {
+                TempData["Error"] = "User Not Found";
+                return Redirect("/login/forgetPassword");
+            }
 
+            string key = "770A8A65DA156D24EE2A093277530142";
+            String encryptedEmail = _petientfunctionrepo.Encrypt(email , key);
+            DateTime date = DateTime.Now;
+            string encryptedDate = _petientfunctionrepo.Encrypt(date.ToString(), key);
+            var resetPassowrdLink = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/Login/changePassword?email={encryptedEmail}&datetime={encryptedDate}";
+            _petientfunctionrepo.SendEmail(email, resetPassowrdLink);
+            return Ok();
+        }
+
+        public IActionResult changePassword()
+        {
+            string encryptedEmail = Request.Query["email"];
+            string encryptedDatetime = Request.Query["datetime"];
+
+            string key = "770A8A65DA156D24EE2A093277530142";
+            string email = _petientfunctionrepo.Decrypt(encryptedEmail, key);
+            string datetime = _petientfunctionrepo.Decrypt(encryptedDatetime, key);
+
+            DateTime getDate = DateTime.Parse(datetime);
+            DateTime currentDate = DateTime.Now;
+
+            var diffOfDate = currentDate - getDate;
+
+            if (diffOfDate.TotalSeconds < 1200) {
+                return View();
+            }
+            else
+            {
+                TempData["Error"] = "Link Expired Please Create New Link";
+                return Redirect("/Login/forgetPassword");
+            }
+        }
     }
 }
