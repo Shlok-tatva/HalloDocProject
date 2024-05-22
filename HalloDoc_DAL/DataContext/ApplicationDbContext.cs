b8095e9a5fc485eecc38473556051ac4412f27e5
Shlok-tatva <shlok.jadeja@etatvasoft.com>
2024-05-22 10:29:36 +0530

    Live Database Added into Project

--- HalloDoc_DAL/DataContext/ApplicationDbContext.cs
+++ HalloDoc_DAL/DataContext/ApplicationDbContext.cs
@@ -88,7 +88,9 @@ public partial class ApplicationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("User ID = postgres;Password=shlok123;Server=localhost;Port=5432;Database=HelloDoc;Integrated Security=true;Pooling=true;");
+    //=> optionsBuilder.UseNpgsql("User ID = postgres;Password=shlok123;Server=localhost;Port=5432;Database=HelloDoc;Integrated Security=true;Pooling=true;");
+    => optionsBuilder.UseNpgsql("User Id=postgres.mqoospbpezpknhvgxrwk;Password=ShlokJadeja@#177;Server=aws-0-ap-south-1.pooler.supabase.com;Port=5432;Database=postgres;");
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
